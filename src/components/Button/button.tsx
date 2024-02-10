b3d241d4718cf3981c9f9803c12b96326cfd110f
Vedant Jain <jscafe.business@gmail.com>
2024-02-10 11:06:45 +0530

    evil commit

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -3,6 +3,10 @@ interface ButtonProps {
 }
 export const Button = (props: ButtonProps) => {
     const { onClick } = props
+    const handler = () => {
+        console.log("hey")
+    }
+    if (!onClick) return <button onClick={onClick} data-testid="button-test">button</button>
     return (
         <button onClick={onClick} data-testid="button-test">button</button>
     )
