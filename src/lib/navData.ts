419dc53d1385aec6b09755c717b091f6609c7575
Vedant Jain <jscafe.business@gmail.com>
2024-03-08 13:18:48 +0530

    Add Navbar

---
+++ src/lib/navData.ts
@@ -0,0 +1,45 @@
+export const navData = [
+  {
+    id: 0,
+    items: [
+      {
+        id: 0,
+        type: "logo",
+        content: "JS CAFE",
+      },
+    ],
+  },
+  {
+    id: 1,
+    items: [
+      {
+        id: 1,
+        type: "item",
+        content: "Your Work",
+      },
+      {
+        id: 2,
+        type: "item",
+        content: "Projects",
+      },
+      {
+        id: 3,
+        type: "item",
+        content: "Filters",
+      },
+    ],
+  },
+  {
+    id: 2,
+    items: [
+      {
+        id: 1,
+        type: "avatar",
+      },
+      {
+        id: 2,
+        type: "themToggle",
+      },
+    ],
+  },
+];
