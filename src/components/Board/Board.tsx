185293fa787dba8b438992de60fdc1e393a8d1c7
Vedant Jain <jscafe.business@gmail.com>
2024-03-21 17:31:18 +0530

    Add Drag & Drop Feature

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,6 +1,10 @@
+"use client"
 import type { BoardColumn, BoardTicket, User } from "@prisma/client"
+import { DragDropContext, Droppable } from '@hello-pangea/dnd';
+import type { OnDragEndResponder } from '@hello-pangea/dnd'
 import { Ticket } from "./index"
-
+import { useState } from "react";
+import cx from 'classnames'
 interface BoardTicketWithUser extends BoardTicket {
     assignedUser: {
         name: string;
@@ -15,16 +19,44 @@ interface BoardProps {
 
 const Board = (props: BoardProps) => {
     const { boardColumns, boardTickets } = props
+    const [tickets, setTickets] = useState(boardTickets)
     const fetTicketForColumn = (columnId: string) => {
-        return boardTickets.filter((ticket) => ticket.boardColumnId === columnId)
+        return tickets.filter((ticket) => ticket.boardColumnId === columnId).sort((a, b) => a.position - b.position)
+    }
+
+    const handleDragEnd: OnDragEndResponder = (result) => {
+        console.log("vedant drag end", result)
+        const currentTickets = JSON.parse(JSON.stringify(tickets))
+        const updatedTickets = currentTickets.map((ticket: BoardTicketWithUser) => {
+            if (ticket.id === result.draggableId) {
+                return { ...ticket, boardColumnId: result.destination?.droppableId, position: result.destination?.index }
+            } else if (ticket.boardColumnId === result.destination?.droppableId && ticket.position >= result.destination?.index) {
+                return { ...ticket, position: ticket.position + 1 }
+            } else if (ticket.boardColumnId === result.source.droppableId && ticket.position > result.source.index) {
+                return { ...ticket, position: ticket.position - 1 }
+            }
+            return ticket
+        })
+        setTickets(updatedTickets)
     }
     return (
-        <div className="flex flex-col sm:flex-row gap-3">
-            {boardColumns.map((column) => <div key={column.id} className="grow basis-full background-neutral-static p-2">
-                <div className="text-subtlest font-semibold text-sm mb-5">{column.label}</div>
-                <div>{fetTicketForColumn(column.id).map((ticket) => <Ticket key={ticket.id} title={ticket.title} assignee={ticket.assignedUser.name} storyPoints={ticket.storyPoints} />)}</div>
-            </div>)}
-        </div>
+        <DragDropContext
+            onDragEnd={handleDragEnd}
+        >
+            <div className="flex flex-col sm:flex-row gap-3">
+                {boardColumns.map((column) => (
+                    <Droppable key={column.id} droppableId={column.id}>
+                        {(provided, snapshot) => (
+                            <div ref={provided.innerRef} className={cx("grow basis-full background-neutral-static p-2", { ['background-board-drop']: snapshot.isDraggingOver })} {...provided.droppableProps}>
+                                <div className="text-subtlest font-semibold text-sm mb-5">{column.label}</div>
+                                <div>{fetTicketForColumn(column.id).map((ticket, index) => <Ticket key={ticket.id} id={ticket.id} index={index} title={ticket.title} assignee={ticket.assignedUser.name} storyPoints={ticket.storyPoints} />)}</div>
+                            </div>
+                        )}
+                    </Droppable>
+                )
+                )}
+            </div>
+        </DragDropContext >
     )
 }
 
