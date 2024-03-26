7133b29b96a2de931ce59030d1d5add8dbb50a89
Vedant Jain <jscafe.business@gmail.com>
2024-03-26 15:41:38 +0530

    Add form validations & Fetch users

--- src/components/Nav/navCreate.tsx
+++ src/components/Nav/navCreate.tsx
@@ -1,5 +1,6 @@
 "use client"
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
+import { useForm, SubmitHandler, Controller } from "react-hook-form"
 import {
     Button,
     Dialog,
@@ -11,14 +12,61 @@ import {
     Select,
     Option
 } from "@material-tailwind/react";
-
+import { zodResolver } from '@hookform/resolvers/zod';
 import { Editor } from '@tinymce/tinymce-react';
+import * as z from 'zod';
+import { getUsers } from "@/app/actions/users";
+import { User } from "@prisma/client";
+
+interface CreateTicketFields {
+    title: string,
+    assignee: string,
+    description: string,
+    points: number
+}
+
+const schema = z.object({
+    title: z.string().min(5),
+    assignee: z.string(),
+    description: z.string().optional(),
+    points: z.number().optional(),
+});
 
 const NavCreate = () => {
     const [open, setOpen] = useState(false);
+    const [users, setUsers] = useState<User[]>([])
     const handleOpen = () => setOpen(!open);
     const modalRef = useRef(null)
     const editorRef = useRef(null);
+    const {
+        register,
+        handleSubmit,
+        watch,
+        control,
+        setValue,
+        formState: { errors },
+    } = useForm<CreateTicketFields>({
+        resolver: zodResolver(schema),
+        defaultValues: {
+            title: "",
+            description: "",
+            points: 0
+        }
+    })
+
+    const onSubmit: SubmitHandler<CreateTicketFields> = (data) => {
+        console.log(data)
+    }
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            const usersRes = await getUsers()
+            console.log(usersRes)
+            setUsers(usersRes)
+        }
+        fetchUsers()
+    }, [])
+    console.log(errors)
     return (
         <>
             <button className="button-create text-black p-2 border rounded font-semibold border-none" onClick={handleOpen}>
@@ -26,38 +74,63 @@ const NavCreate = () => {
             </button>
             <Dialog open={open} handler={handleOpen} placeholder="Create ticket" ref={modalRef} dismiss={{ enabled: false }}>
                 <DialogHeader placeholder="Create ticket">Create a ticket</DialogHeader>
-                <DialogBody placeholder="Create ticket">
-                    <form>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <DialogBody placeholder="Create ticket">
                         <Typography variant="h4" color="blue-gray" placeholder={undefined}>
                             Enter ticket title
                         </Typography>
-                        <Input name="title" placeholder="Enter ticket title" crossOrigin={undefined} />
+                        <Controller
+                            name="title"
+                            control={control}
+                            render={({ field }) => (
+                                <Input placeholder="Enter ticket title" crossOrigin={undefined} {...field} />
+                            )} />
+                        <Typography variant="h6" placeholder={undefined} className="mt-3 text-red-600">
+                            {errors.title?.message}
+                        </Typography>
                         <Typography variant="h4" color="blue-gray" placeholder={undefined}>
                             Assignee
                         </Typography>
-                        <Select
-                            placeholder="USA"
-                            className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
-                            labelProps={{
-                                className: "before:content-none after:content-none",
-                            }}
-                            menuProps={{ className: "h-48" }}
-                        >
-                            {["John", "Kane"].map((user, index) => (
-                                <Option key={index} value={user}>
-                                    <div className="flex items-center gap-x-2">
-                                        {user}
-                                    </div>
-                                </Option>
-                            ))}
-                        </Select>
+                        <Controller
+                            name="assignee"
+                            control={control}
+                            render={({ field }) => (
+                                <Select
+                                    placeholder="USA"
+                                    className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+                                    labelProps={{
+                                        className: "before:content-none after:content-none",
+                                    }}
+                                    menuProps={{ className: "h-48" }}
+                                    {...field}
+                                >
+                                    {users.map((user, index) => (
+                                        <Option key={index} value={user.id}>
+                                            <div className="flex items-center gap-x-2">
+                                                {user.name}
+                                            </div>
+                                        </Option>
+                                    ))}
+                                </Select>
+                            )} />
+                        <Typography variant="h6" placeholder={undefined} className="mt-3 text-red-600">
+                            {errors.assignee?.message}
+                        </Typography>
                         <Typography variant="h4" color="blue-gray" placeholder={undefined} className="mt-3">
                             Enter ticket description
                         </Typography>
+                        <input
+                            id="description"
+                            name="description"
+                            style={{ display: "none" }}
+                        />
                         <Editor
                             apiKey={process.env.NEXT_PUBLIC_TINY_KEY}
                             // @ts-ignore
                             onInit={(evt, editor) => editorRef.current = editor}
+                            onEditorChange={(newValue, editor) =>
+                                setValue("description", newValue)
+                            }
                             initialValue="<p>This is the initial content of the editor.</p>"
                             init={{
                                 height: 400,
@@ -86,27 +159,32 @@ const NavCreate = () => {
                                 content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
                             }}
                         />
+                        <Typography variant="h6" placeholder={undefined} className="mt-3 text-red-600">
+                            {errors.description?.message}
+                        </Typography>
                         <Typography variant="h4" color="blue-gray" className="mt-3" placeholder={undefined}>
                             Story Points
                         </Typography>
-                        <Input name="points" placeholder="Story Point" type="number" crossOrigin={undefined} />
-
-                    </form>
-                </DialogBody>
-                <DialogFooter placeholder="Create ticket">
-                    <Button
-                        placeholder="Create ticket"
-                        variant="text"
-                        color="red"
-                        onClick={handleOpen}
-                        className="mr-1"
-                    >
-                        <span>Cancel</span>
-                    </Button>
-                    <Button placeholder="Create ticket" variant="gradient" color="green" onClick={handleOpen}>
-                        <span>Confirm</span>
-                    </Button>
-                </DialogFooter>
+                        <Input placeholder="Story Point" crossOrigin={undefined} type="number" {...register('points', { valueAsNumber: true })} />
+                        <Typography variant="h6" placeholder={undefined} className="mt-3 text-red-600">
+                            {errors.points?.message}
+                        </Typography>
+                    </DialogBody>
+                    <DialogFooter placeholder="Create ticket">
+                        <Button
+                            placeholder="Create ticket"
+                            variant="text"
+                            color="red"
+                            onClick={handleOpen}
+                            className="mr-1"
+                        >
+                            <span>Cancel</span>
+                        </Button>
+                        <Button type="submit" placeholder="Create ticket" variant="gradient" color="green">
+                            <span>Confirm</span>
+                        </Button>
+                    </DialogFooter>
+                </form>
             </Dialog>
         </>
     )
